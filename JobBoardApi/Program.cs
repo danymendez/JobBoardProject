ac26b61777756dda271d33c22d68995da8ca57ae
Daniel Méndez <36239306+danymendez@users.noreply.github.com>
2020-09-08 17:04:19 +0000

    Fixing some configuration in Program.cs Api

--- JobBoardApi/Program.cs
+++ JobBoardApi/Program.cs
@@ -16,20 +16,14 @@ namespace JobBoardApi
     {
         public static void Main(string[] args)
         {
-         CreateHostBuilder(args).Build();
-           
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            
-
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-             
-        
-
     }
 }
